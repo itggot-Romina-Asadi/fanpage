4089d35ef226781ddc64c74f6aa4165790c9fc22
itggot-Romina-Asadi <romina.asadi@itggot.se>
2017-05-08 11:18:39 +0200

    Gjorde ändringar på startsidan
    - Gjorde alla puzzelbilder B&W
    - La till en post-it i headern
    - Ändrade menyikonen

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,12 @@
 /**
  * Created by romina.asadi on 2017-04-24.
  */
+function img_over() {
+    document.querySelector(".post-it").classList.add("remove");
+    document.querySelector(".post-it-hover").classList.remove("remove");
+}
+
+function img_out() {
+    document.querySelector(".post-it").classList.remove("remove");
+    document.querySelector(".post-it-hover").classList.add("remove");
+}
